e3563ec9f0ee36bd95e6a1a765546c5ccb3cebcf
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-17 21:02:22 -0300

    users entity

---
+++ src/app/users/entities/users.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'first_name' })
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  age: number;
+
+  @Column({ name: 'cell_phone' })
+  cellPhone: string;
+
+  @CreateDateColumn({ name: 'creaed_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
+}
