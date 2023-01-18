ea0082a41e9de536f3ebf072fb8134681bc2aaee
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-17 21:10:30 -0300

    users entity association and method getAll

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/users.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
 })
 export class UsersModule {}
--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './entities/users.entity';
 
 @Injectable()
 export class UsersService {
-  //constructor() {}
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersReoistory: Repository<UserEntity>,
+  ) {}
+
+  async findAll() {
+    return await this.usersReoistory.find();
+  }
 }
